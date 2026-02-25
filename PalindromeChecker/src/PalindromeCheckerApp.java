5ea18cccca49ca678d2160fd188aff45c6593dec
Krishna-Kuntal <kk4773@srmist.edu.in>
2026-02-25 14:18:57 +0530

    UC1: Add welcome message module

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,5 +1,13 @@
 public class PalindromeCheckerApp {
+    // UC1: Welcome Message
+    public static void displayWelcome() {
+        System.out.println("=================================");
+        System.out.println(" PALINDROME CHECKER APPLICATION ");
+        System.out.println(" Version 1.0 ");
+        System.out.println("=================================");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Application Started");
+        displayWelcome();
     }
 }
