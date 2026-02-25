67a837a3a6dfcd00c934c83a08d3410f431fccef
Krishna-Kuntal <kk4773@srmist.edu.in>
2026-02-25 14:54:48 +0530

    UC6:Menu Driven System

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -48,9 +48,35 @@ public class PalindromeCheckerApp {
         }
     }
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         displayWelcome();
-        hardcodedPalindrome();
-        userInputPalindrome();
-        advancedPalindrome();
+
+        while (true) {
+            System.out.println("\n1. Hardcoded");
+            System.out.println("2. User Input");
+            System.out.println("3. Advanced");
+            System.out.println("4. Exit");
+            System.out.print("Choose option: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    hardcodedPalindrome();
+                    break;
+                case 2:
+                    userInputPalindrome();
+                    break;
+                case 3:
+                    advancedPalindrome();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
     }
 }
